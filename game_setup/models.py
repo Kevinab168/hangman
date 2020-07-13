aac2b502e7dae5cd09fc27befd03826c195ad37d
Kevin <kevin.abraham1688@gmail.com>
2020-07-13 13:30:03 -0400

    Add Make a guess functionality
    - Fix flake8

--- game_setup/models.py
+++ game_setup/models.py
@@ -7,4 +7,12 @@ class User(AbstractUser):
 
 
 class Game(models.Model):
+    winning_word = models.CharField(max_length=200)
+    attempts_left = models.IntegerField(default=6, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    in_progress = models.BooleanField(default=True)
+
+
+class Guess(models.Model):
+    guess_value = models.CharField(max_length=1)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
